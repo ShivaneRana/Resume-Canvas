aa2a7bf1ccf65f514cbfa862616adb81c43f86e8
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-10 22:46:51 +0530

    feat: component created

---
+++ src/components/editor.jsx
@@ -0,0 +1,11 @@
+import "../styles/editor.css";
+
+function Editor(){
+    return(
+        <div className="editor">
+           Editor 
+        </div>
+    )
+}
+
+export default Editor;
---
+++ src/components/screen.jsx
@@ -0,0 +1,11 @@
+import "../styles/screen.css"
+
+function Screen(){
+    return(
+        <div className="screen">
+            Screen
+        </div>
+    )
+}
+
+export default Screen;
