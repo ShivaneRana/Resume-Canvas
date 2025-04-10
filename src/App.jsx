779da7adb2af19a5a723547ea62cd178dfbaf6d7
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-10 22:47:53 +0530

    feat: editor and screen component added to container div

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
 import Navbar from './components/navbar.jsx';
+import Editor from './components/editor.jsx';
+import Screen from './components/screen.jsx';
+
 
 function App() {
   return (
     <div className='app'>
         <Navbar></Navbar>
+        <div className='container'>
+          <Editor></Editor> 
+          <Screen></Screen>
+        </div>
     </div>
   )
 };
