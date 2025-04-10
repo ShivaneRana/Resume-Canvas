c40a02c7189df3fecdfa85c5afb62121d43048f1
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-10 21:20:06 +0530

    feat: basic structure added

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Lenis from 'lenis'
+import './index.css'
+import App from './App.jsx'
+
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
