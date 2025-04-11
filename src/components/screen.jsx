0335da33e2aed994269db31c6f12189ff0c9a8a9
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-11 16:16:11 +0530

    Resume added to screen

--- src/components/screen.jsx
+++ src/components/screen.jsx
@@ -1,8 +1,10 @@
 import "../styles/screen.css"
+import Resume from "../components/resume.jsx";
 
 function Screen(){
     return(
         <div className="screen">
+            <Resume></Resume>
         </div>
     )
 }
