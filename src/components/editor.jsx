d6b643c749d8346dad6c7072b646e30dc4fb6ef7
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-11 19:39:20 +0530

    feat: button added in form of images

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -1,9 +1,29 @@
 import "../styles/editor.css";
+import "../styles/utility.css";
+import addIcon from "../assets/images/add.svg";
+import clearIcon from "../assets/images/clear.svg";
+import copyIcon from "../assets/images/copy.svg";
+import deleteIcon from "../assets/images/bin.svg";
+import exampleIcon from "../assets/images/example.svg";
+import printIcon from "../assets/images/print.svg";
+
 
 function Editor(){
     return(
         <div className="editor">
-        </div>
+            <div className="buttonHolder">
+                <div className="resumeSlots">
+                </div>
+                <div className="resumeButtons">
+                    <img className="defaultButton" src = {addIcon}></img>
+                    <img className="defaultButton" src = {copyIcon}></img>
+                    <img className="defaultButton" src = {exampleIcon}></img>
+                    <img className="defaultButton" src = {clearIcon}></img>
+                    <img className="defaultButton" src = {deleteIcon}></img>
+                    <img className="defaultButton" src = {printIcon}></img>
+                </div>
+            </div>
+       </div>
     )
 }
 
