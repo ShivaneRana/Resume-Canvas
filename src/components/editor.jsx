6e78a96045ed59593b1fa18d681f925a9c16b18e
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-14 18:38:03 +0530

    Toplayer function created

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -1,8 +1,22 @@
+// styles
 import style from "../styles/editor.module.css";
 
+//assets
+
+// represents the entire editor side
 function Editor(){
     return(
         <div className={style.mainContainer}>
+        </div>
+    )
+}
+
+// holds the top layer ( resume slots and action button)
+function TopLayer(){
+    return(
+        <div>
+            <div></div>
+            <div></div>
         </div>
     )
 }
