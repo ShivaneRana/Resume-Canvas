d16e733ddf38c84cc6cd5da4f0f56950f2954615
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-14 22:52:10 +0530

    Header component added

---
+++ src/components/personalDetail.jsx
@@ -0,0 +1,35 @@
+// styles
+import style from "../styles/personalDetail.module.css";
+
+// assets
+import arrow from "../assets/images/arrow.svg";
+import showIcon from "../assets/images/show.svg";
+import hideIcon from "../assets/images/hide.svg";
+import { useState } from "react";
+
+function PersonalDetail(){
+    return(
+        <div className={style.mainContainer}>
+           <Header></Header> 
+        </div>
+    )
+}
+
+function Header(){
+    const [expanded,setExpanded] = useState(true);
+
+    return(
+        <div className={style.header}>
+            <div>
+                <button><img src={arrow}></img></button>
+                <h2>Personal Details: </h2>
+            </div>
+            <div>
+                <button><img src={showIcon}></img></button> 
+            </div>
+        </div>
+    )
+}
+
+
+export default PersonalDetail;
