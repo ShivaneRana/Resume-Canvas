a2cb875689f6555462c56286350fd51194ca5fc9
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-14 16:05:07 +0530

    Content component added to App.jsx and styling too

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className={style.mainContainer}>
         <Navbar></Navbar>
+        <Content></Content>        
+    </div>
+  )
+}
+
+function Content(){
+  return(
+    <div className={style.content}>
+
+
     </div>
   )
 }
