3d995602363826c6d8216a394d8ed55e52d60345
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-14 21:26:52 +0530

    all icon now inclosed in a button

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -2,21 +2,36 @@
 import style from "../styles/editor.module.css";
 
 //assets
+import addIcon from "../assets/images/add.svg"
+import copyIcon from "../assets/images/copy.svg"
+import exampleIcon from "../assets/images/example.svg"
+import clearIcon from "../assets/images/clear.svg"
+import deleteIcon from "../assets/images/delete.svg"
+import printIcon from "../assets/images/print.svg"
+
 
 // represents the entire editor side
 function Editor(){
     return(
         <div className={style.mainContainer}>
+            <TopLayer></TopLayer>
         </div>
     )
 }
 
-// holds the top layer ( resume slots and action button)
+// holds the top layer ( resume slots and buttonSlot ( add, copy , example etc) )
 function TopLayer(){
     return(
-        <div>
-            <div></div>
-            <div></div>
+        <div className={style.topLayer}>
+            <div className={style.resumeSlot}></div>
+            <div className={style.buttonSlot}>
+                <button><img src={addIcon}></img></button>
+                <button><img src={copyIcon}></img></button>
+                <button><img src={exampleIcon}></img></button>
+                <button><img src={clearIcon}></img></button>
+                <button><img src={deleteIcon}></img></button>
+                <button><img src={printIcon}></img></button>
+            </div>
         </div>
     )
 }
