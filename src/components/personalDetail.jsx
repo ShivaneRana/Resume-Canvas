c6555e792d549e6b66756e6a09b43628eca2b25f
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-15 11:06:27 +0530

    basic functionality added

--- src/components/personalDetail.jsx
+++ src/components/personalDetail.jsx
@@ -2,35 +2,162 @@
 import style from "../styles/personalDetail.module.css";
 
 // assets
-import arrow from "../assets/images/arrow.svg";
+import expandIcon from "../assets/images/expand.svg";
+import shrinkICon from "../assets/images/shrink.svg";
 import showIcon from "../assets/images/show.svg";
 import hideIcon from "../assets/images/hide.svg";
 import { useState } from "react";
 
 function PersonalDetail() {
+  const [expanded, setExpanded] = useState(true);
+
+  function toggleExpanded() {
+    setExpanded(!expanded);
+  }
+
   return (
     <div className={style.mainContainer}>
-      <Header></Header>
+      <Header func={toggleExpanded} isExpanded={expanded}></Header>
+      {expanded && <Content></Content>}
     </div>
   );
 }
 
-function Header() {
-  const [expanded, setExpanded] = useState(true);
+function Header({ func,isExpanded}) {
+ 
+  let icon = isExpanded ? shrinkICon : expandIcon;
 
   return (
     <div className={style.header}>
       <div>
-        <button>
-          <img alt="expand/collapse icon" src={arrow}></img>
+        <button onClick={func}>
+          <img onClick = {(e) => e.target.classList.toggle} alt="expand/collapse icon" src={icon}></img>
         </button>
-        <h2>Personal Details: </h2>
+        <h2 onClick={func}>Personal Details: </h2>
       </div>
       <div>
+          <DisplayButton></DisplayButton>
+      </div>
+    </div>
+  );
+}
+
+function Content() {
+  return (
+    <div className={style.content}>
+      <FullName></FullName>
+      <Email></Email>
+      <PhoneNumber></PhoneNumber>
+      <Address></Address>
+      <Github></Github>
+      <Linkedin></Linkedin>
+      <PersonalWebsite></PersonalWebsite>
+    </div>
+  );
+}
+
+function DisplayButton(){
+    return(
         <button>
           <img alt="show/hide icon" src={showIcon}></img>
         </button>
-      </div>
+    )
+}
+
+function FullName() {
+  return (
+    <div className={style.defaultDiv}>
+      <h3>Full Name: </h3>
+      <label htmlFor="fullname"></label>
+      <input
+        name="fullname"
+        type="text"
+        placeholder="Enter fullname"
+      ></input>
+    </div>
+  );
+}
+
+function Address(){
+return (
+    <div className={style.defaultDiv}>
+      <h3>Address : </h3>
+      <label htmlFor="address"></label>
+      <input
+        name="address"
+        type="text"
+        placeholder="Enter address"
+      ></input>
+    </div>
+  );
+}
+
+function PhoneNumber(){
+return (
+    <div className={style.defaultDiv}>
+      <h3>Phone Number : </h3>
+      <label htmlFor="phonenumber"></label>
+      <input
+        name="phonenumber"
+        type="tel"
+        placeholder="Enter your personal number"
+      ></input>
+    </div>
+  );
+}
+
+function Email(){
+return (
+    <div className={style.defaultDiv}>
+      <h3>Email: </h3>
+      <label htmlFor="email"></label>
+      <input
+        name="email"
+        type="email"
+        placeholder="Enter your email"
+      ></input>
+    </div>
+  );
+}
+
+function Github(){
+return (
+    <div className={style.defaultDiv}>
+      <h3>Github: </h3>
+      <label htmlFor="github"></label>
+      <input
+        name="github"
+        type="url"
+        placeholder="Enter your github profile url"
+      ></input>
+    </div>
+  );
+}
+
+function Linkedin(){
+return (
+    <div className={style.defaultDiv}>
+      <h3>Linkedin: </h3>
+      <label htmlFor="linkedin"></label>
+      <input
+        name="linkedin"
+        type="url"
+        placeholder="Enter your linkedin profile url"
+      ></input>
+    </div>
+  );
+}
+
+function PersonalWebsite(){
+return (
+    <div className={style.defaultDiv}>
+      <h3>Personal website: </h3>
+      <label htmlFor="personalwebsite"></label>
+      <input
+        name="personalwebsite"
+        type="url"
+        placeholder="Enter your personal website url"
+      ></input>
     </div>
   );
 }
