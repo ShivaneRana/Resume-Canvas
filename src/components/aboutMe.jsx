42c6ca1e0fe698f941d63ed70282b431b8f345b2
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-15 20:26:10 +0530

    basic functionality completed

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -2,16 +2,65 @@
 import style from "../styles/aboutMe.module.css";
 
 //assets
-import 
+import expandIcon from "../assets/images/expand.svg";
+import shrinkICon from "../assets/images/shrink.svg";
+import showIcon from "../assets/images/show.svg";
+import hideIcon from "../assets/images/hide.svg";
 
 //components
-
-
+import { useState } from "react";
 
 function AboutMe(){
-    return(
-        <div>
+  const [expanded, setExpanded] = useState(true);
 
+  function toggleExpanded() {
+    setExpanded(!expanded);
+  }
+
+    return(
+        <div className={style.mainContainer}>
+            <Header func={toggleExpanded} isExpanded={expanded}></Header>
+            {expanded && <Content></Content>}
         </div>
     )
 }
+
+function Header({func,isExpanded}){
+
+    let icon = isExpanded ? expandIcon : shrinkICon;
+
+    return(
+        <div className={style.header}>
+            <div>
+                <button onClick={func}>
+                <img alt="expand/collapse icon" src={icon}></img>
+                </button>
+                <h2 onClick={func}>About me: </h2>
+            </div>
+            <div>
+                <DisplayButton></DisplayButton>
+            </div>
+        </div>
+    )
+}
+
+function Content(){
+    return(
+        <div className={style.content}>
+            <label htmlFor="objective"></label>
+            <textarea name="objective" placeholder="Enter description about yourself">
+
+            </textarea>
+        </div>
+    )
+}
+
+function DisplayButton(){
+    return(
+        <button>
+          <img alt="show/hide icon" src={showIcon}></img>
+        </button>
+    )
+}
+
+export default AboutMe;
