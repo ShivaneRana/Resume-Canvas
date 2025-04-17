2698c5230957593448feae63a01e760d8e7badb7
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-17 13:30:03 +0530

    new component created

---
+++ src/components/skillDetails.jsx
@@ -0,0 +1,11 @@
+import style from "../styles/skillDetails.module.css";
+
+function SkillDetails(){
+    return(
+        <div className={style.mainContainer}>
+            
+        </div>
+    )
+}
+
+export default SkillDetails;
