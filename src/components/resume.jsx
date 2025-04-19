6e9c82e4dd3a497e7dce2b0ed927b6248ea86820
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-19 14:54:08 +0530

    basic component added

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -9,6 +9,11 @@ function Resume() {
         <SocialLinks></SocialLinks>
       </div>
       <AboutMe></AboutMe>
+      <Skill></Skill>
+      <Education></Education>
+      <Project></Project>
+      <WorkExperience></WorkExperience>
+      <Additional></Additional>
     </div>
   </div>
   );
@@ -40,8 +45,59 @@ function AboutMe(){
     <div className={style.aboutMeDiv}>
       <h2>About me</h2>
       <hr className={style.line}></hr>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.
+        Sed cursus, sem in tincidunt lacinia, lectus nisi pulvinar erat, ut dictum
+        sapien nisi nec est. Pellentesque habitant morbi tristique senectus et netus
+        et malesuada fames ac turpiitor.   
+      </p>
     </div>
   )
 }
 
+function Skill(){
+  return(
+    <div className={style.skillDiv}>
+      <h2>Skill</h2> 
+      <hr className={style.line}></hr>
+
+    </div>
+  )
+}
+
+function Education(){
+  return(
+    <div className={style.educationDiv}>
+      <h2>Education</h2>
+      <hr className={style.line}></hr>
+    </div>
+  )
+}
+
+function Project(){
+  return(
+    <div className={style.projectDiv}>
+      <h2>Project</h2>
+      <hr className={style.line}></hr>
+    </div>
+  )
+}
+
+function WorkExperience(){
+  return(
+    <div className={style.workExperienceDiv}>
+      <h2>Work Experience</h2>
+      <hr className={style.line}></hr>
+    </div>
+  )
+}
+
+function Additional(){
+  return(
+    <div className={style.additionalDiv}>
+      <h2>Additional</h2>
+      <hr className={style.line}></hr> 
+    </div>
+  )
+}
 export default Resume;
