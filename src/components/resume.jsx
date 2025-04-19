0675593fa9dca1dd1f1a32197e9f4675ffc771ba
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-19 13:22:09 +0530

    minor changes

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,7 +1,18 @@
 import style from "../styles/resume.module.css";
 
 function Resume() {
-  return <div className={style.mainContainer}></div>;
+  return(
+  <div className={style.mainContainer}>
+    <Name></Name>
+  </div>
+  );
+}
+
+function Name(){
+  return(
+    <h1>
+    </h1>
+  )
 }
 
 export default Resume;
