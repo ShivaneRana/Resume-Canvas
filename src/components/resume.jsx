0911e78b78de957d4074439edba3bcc5ace339df
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-19 13:57:32 +0530

    component added for displaying info

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -3,15 +3,44 @@ import style from "../styles/resume.module.css";
 function Resume() {
   return(
   <div className={style.mainContainer}>
-    <Name></Name>
+    <div>
+      <div>
+        <Name></Name>
+        <SocialLinks></SocialLinks>
+      </div>
+      <AboutMe></AboutMe>
+    </div>
   </div>
   );
 }
 
 function Name(){
   return(
-    <h1>
-    </h1>
+    <div className={style.nameDiv}>
+      <h1>Shivane Rana</h1>
+    </div>
+  )
+}
+
+function SocialLinks(){
+  return(
+    <div className={style.socialLinksDiv}>
+      <a href="www.google.com" target="blank">Google</a>
+      <a href="www.google.com" target="blank">Google</a>
+      <a href="www.google.com" target="blank">Google</a>
+      <a href="www.google.com" target="blank">Google</a>
+      <a href="www.google.com" target="blank">Google</a>
+      <a href="www.google.com" target="blank">Google</a>
+    </div>
+  )
+}
+
+function AboutMe(){
+  return(
+    <div className={style.aboutMeDiv}>
+      <h2>About me</h2>
+      <hr className={style.line}></hr>
+    </div>
   )
 }
 
