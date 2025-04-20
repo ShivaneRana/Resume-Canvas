fe0a660832cdc05eb615a0258264b1a0e828275f
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-20 14:48:45 +0530

    log clicked resume seprately

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -51,6 +51,8 @@ function ResumeSlot() {
         return(
           <button
           onClick={() => {
+            console.log("Clicked resume~");
+            console.log(resume);
             context.changeActiveResume(resume); 
           }}
           key={resume.id}>
