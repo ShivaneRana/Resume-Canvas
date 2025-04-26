1ec649b3d75c6d8464c00c8dde5104e3cab92cef
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-26 20:22:33 +0530

    added skill properties

--- src/App.jsx
+++ src/App.jsx
@@ -179,6 +179,11 @@ const exampleTemplate = {
   },
   aboutMe:
     "Detail-oriented full-stack developer with over 6 years of experience designing and implementing modern web applications. Adept at building responsive front-ends using React and managing scalable APIs with Node.js and Express. Strong advocate for accessible design and test-driven development. Experienced in Agile workflows and collaborating with cross-functional teams to deliver user-focused solutions.",
+  skill:{
+    "technical skill":["vscode","github","docker","kubernetes"],
+    "backend skill":["postgresql","sql","node.js","express.js"],
+    "frontend skill":["react","next.js","nest.js","gsap"],
+  },
 };
 
 const baseTemplate = {
@@ -192,6 +197,7 @@ const baseTemplate = {
     personalWebsite: "",
   },
   aboutMe: "",
+  skill:{},
 };
 
 export default App;
