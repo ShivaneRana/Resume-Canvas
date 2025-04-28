bbe2c8587b3e42ec6c50754c2b5be14dfb6711ff
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-04-28 22:34:27 +0530

    Skill key added to baseTemplate and exampleTemplate

--- src/App.jsx
+++ src/App.jsx
@@ -190,7 +190,12 @@ const exampleTemplate = {
     personalWebsite: "https://elenam.dev",
   },
   aboutMe:
-    "Detail-oriented full-stack developer with over 6 years of experience designing and implementing modern web applications. Adept at building responsive front-ends using React and managing scalable APIs with Node.js and Express. Strong advocate for accessible design and test-driven development. Experienced in Agile workflows and collaborating with cross-functional teams to deliver user-focused solutions."
+    "Detail-oriented full-stack developer with over 6 years of experience designing and implementing modern web applications. Adept at building responsive front-ends using React and managing scalable APIs with Node.js and Express. Strong advocate for accessible design and test-driven development. Experienced in Agile workflows and collaborating with cross-functional teams to deliver user-focused solutions.",
+  skill: {
+    "technical skill":["vscode", "github", "docker", "kubernetes"],
+    "backend skill":["postgresql", "sql", "node.js", "express.js"],
+    "frontend skill":["react", "next.js", "nest.js", "gsap"],
+  },
 };
 
 const baseTemplate = {
@@ -203,7 +208,8 @@ const baseTemplate = {
     address: "",
     personalWebsite: "",
   },
-  aboutMe: ""
+  aboutMe: "",
+  skill: {}
 };
 
 export default App;
