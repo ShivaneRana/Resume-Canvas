2d0bdb57f3e52706cc00c90087a25c2938a845b5
shivane <72935198+ShivaneRana@users.noreply.github.com>
2025-05-04 17:57:02 +0530

    education component is now fully functional

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -8,10 +8,12 @@ import showIcon from "../assets/images/show.svg";
 import hideIcon from "../assets/images/hide.svg";
 import addIcon from "../assets/images/add.svg";
 import closeIcon from "../assets/images/close.svg";
+import deleteIcon from "../assets/images/delete.svg";
 
 //components
 import { useState, useEffect, createContext, useContext } from "react";
 import { resumeContext } from "../App.jsx";
+import { v4 as uuidv4 } from "uuid";
 
 const internalContext = createContext();
 
@@ -87,8 +89,7 @@ function Content() {
   return (
     <div className={style.content}>
       <ShowArea></ShowArea>
-      {/* {interanal_context.dialogBoxState && <DialogBox></DialogBox>} */}
-      <DialogBox></DialogBox>
+      {interanal_context.dialogBoxState && <DialogBox></DialogBox>}
     </div>
   );
 }
@@ -111,6 +112,9 @@ function DisplayButton() {
     <div className={style.displayButtonDiv}>
       <button
         onClick={() => {
+          const UUID = uuidv4();
+          context.addEducation(context.activeResumeId,UUID);
+          internal_context.changeCurrentTarget(UUID);
           if (internal_context.expanded === false) {
             internal_context.toggleExpanded();
           }
@@ -135,11 +139,44 @@ function DisplayButton() {
 }
 
 function ShowArea() {
+  const internal_context = useContext(internalContext);
   const context = useContext(resumeContext);
   const index = context.findIndex(context.activeResumeId);
   const resume = context.resumeList[index];
 
-  return <div className={style.showArea}></div>;
+  return <div className={style.showArea}>
+    {
+      resume.education.map((item,itemIndex) => {
+        return <div
+        key={item.id}
+        className={style.tray}
+        >
+          <div
+          className={internal_context.currentTarget === item.id ? style.selected : ""}
+          onClick={() => {
+                internal_context.changeCurrentTarget(item.id);
+                if (internal_context.expanded === false) {
+                  internal_context.toggleExpanded();
+                }
+
+                if (internal_context.dialogBoxState === false) {
+                  internal_context.toggleDialogBoxState();
+                }
+              }}
+          >
+            <h4>{(itemIndex+1)+"."+item.name}</h4>
+          </div>
+          <button title="Delete Record"
+          onClick={() => {
+            context.deleteEducation(context.activeResumeId,item.id);
+          }}
+          >
+            <img src={deleteIcon} alt="delete icon"></img>
+          </button>
+        </div>
+      })
+    }
+  </div>;
 }
 
 function DialogBox() {
@@ -148,42 +185,91 @@ function DialogBox() {
   const index = context.findIndex(context.activeResumeId);
   const resume = context.resumeList[index];
 
+  const currentEducation = resume.education.filter(
+    (item) => item.id === internal_context.currentTarget,
+  )[0];
+  if (!currentEducation) return null;
+
   return (
     <div className={style.dialogBox}>
       <div className={style.topDiv}>
         <div>
           <h4>Institution: </h4>
           <label htmlFor="institution"></label>
-          <input name="institution" placeholder="Enter institution"></input>
+          <input
+          value={currentEducation.name}
+          onChange={(e) => {
+            context.changeEducation(context.activeResumeId,currentEducation.id,"name",e.target.value);
+          }}
+          name="institution" placeholder="Enter institution"></input>
         </div>
         <div>
           <h4>Course: </h4>
           <label htmlFor="course"></label>
-          <input name="course" placeholder="Enter course"></input>
+          <input
+          value={currentEducation.course}
+
+          onChange={(e) => {
+            context.changeEducation(context.activeResumeId,currentEducation.id,"course",e.target.value);
+          }}
+          name="course" placeholder="Enter course"></input>
         </div>
         <div>
           <h4>Major: </h4>
           <label htmlFor="major"></label>
-          <input name="major" placeholder="Enter major"></input>
+          <input
+          value={currentEducation.major}
+
+          onChange={(e) => {
+            context.changeEducation(context.activeResumeId,currentEducation.id,"major",e.target.value);
+          }}
+          name="major" placeholder="Enter major"></input>
         </div>
         {/* holds entire start and end date */}
         <div className={style.dateDiv}>
           <div>
             <h4>Start date: </h4>
             <label htmlFor="startDate"></label>
-            <input name="startDate" placeholder="Enter start date"></input>
+            <input
+            value={currentEducation.startDate}
+
+          onChange={(e) => {
+            context.changeEducation(context.activeResumeId,currentEducation.id,"startDate",e.target.value);
+          }}
+            name="startDate" placeholder="Enter start date"></input>
           </div>
           <div>
             <h4>End date: </h4>
             <label htmlFor="endDate"></label>
-            <input name="endDate" placeholder="Enter end date"></input>
+            <input
+            value={currentEducation.endDate}
+
+          onChange={(e) => {
+            context.changeEducation(context.activeResumeId,currentEducation.id,"endDate",e.target.value);
+          }}
+            name="endDate" placeholder="Enter end date"></input>
           </div>
         </div>
         <div>
           <h4>GPA: </h4>
           <label htmlFor="gpa"></label>
-          <input name="gpa" placeholder="Enter gpa"></input>
+          <input
+          value={currentEducation.gpa}
+
+          onChange={(e) => {
+            context.changeEducation(context.activeResumeId,currentEducation.id,"gpa",e.target.value);
+          }}
+          name="gpa" placeholder="Enter gpa"></input>
         </div>
+      </div>
+      <div className={style.bottomDiv}>
+        <button title="close"
+        onClick={() => {
+          internal_context.toggleDialogBoxState();
+        }}
+        >
+          <img src={closeIcon} alt="close icon"></img>
+        </button>
       </div>
     </div>
   );
